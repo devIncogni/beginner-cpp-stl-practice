2691e5a287651f4bcd194b9ca95a9978ac61ba2b
devIncogni <arinkumar10@outlook.com>
2025-06-13 20:56:57 +0530

    Wrote a function to reverse a number (Also usable for palindrome)

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,26 @@ void numberOfDigitsIn(int N) {
 }
 
 void numberOfDigitsSmartlyIn(int N) {
-  std::cout << std::floor(std::log10(N)) + 1;
+  std::cout << std::floor(std::log10(N)) + 1 << '\n';
 }
+
+void reverseNumber(int N) {
+  int currentDigit{};
+  int sum{0};
+  int i{};
+  while (N > 0) {
+    currentDigit = N % 10;
+    sum = ((sum * 10) + currentDigit);
+    N /= 10;
+  }
+  std::cout << sum << '\n';
+}
+
+
 
 int main() {
   numberOfDigitsIn(54648);
   numberOfDigitsSmartlyIn(54648);
+  reverseNumber(1200540);
   return 0;
 }
