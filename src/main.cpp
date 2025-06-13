b225df7201cf2d3f87adf441f231618751cca735
devIncogni <arinkumar10@outlook.com>
2025-06-13 20:41:43 +0530

    Wrote function to count number of digits in a number

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include "helloworld.h"
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+void numberOfDigitsIn(int N) {
+
+  int counter{};
+  while (N > 0) {
+    int digit{N % 10};
+    N /= 10;
+    counter += 1;
+  }
+  std::cout << counter << '\n';
+}
+
+void numberOfDigitsSmartlyIn(int N) {
+  std::cout << std::floor(std::log10(N)) + 1;
+}
 
 int main() {
-  print("Hello World");
+  numberOfDigitsIn(54648);
+  numberOfDigitsSmartlyIn(54648);
   return 0;
 }
